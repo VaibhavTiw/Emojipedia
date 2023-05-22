920593ed9eae5d109427b487887985d6ed8aadab
VaibhavTiw <vaibhav.tiw543@gmail.com>
2023-05-22 13:49:57 +0530

    First commit

---
+++ src/emojipedia.js
@@ -0,0 +1,47 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+       id: 4,
+       emoji: "😎",
+       name: " Face With Sunglasses",
+       meaning:
+          "It expresses coolness and confidence and is used to convey a carefree, relaxed attitude or to express that something is excellent."
+    },
+    {
+        id: 5,
+        emoji: "😂",
+        name: "Face With Tears of Joy",
+        meaning:
+           "It represents a laughing face with tears of joy, expresses that you can barely hold yourself on the chair with laughter."
+     },
+     {
+         id: 6,
+         emoji: "😍",
+         name: "Smiling Face With Heart-Eyes",
+         meaning:
+            " It conveys intense love and affection for a person, place or even an object. This emoji is often used in romantic messages to express love, gratitude, and admiration."
+      }
+
+  ];
+  
+  export default emojipedia;
